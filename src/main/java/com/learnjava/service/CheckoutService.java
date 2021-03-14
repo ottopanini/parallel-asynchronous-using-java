3bfa299485345aa4e0029f51c143ac1889d4ddcb
Christian Preß <cpress@it4ipm.de>
2021-03-14 09:35:25 +0100

    add sum by reduce

--- src/main/java/com/learnjava/service/CheckoutService.java
+++ src/main/java/com/learnjava/service/CheckoutService.java
@@ -10,8 +10,6 @@ import com.learnjava.util.LoggerUtil;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static java.util.stream.Collectors.summingDouble;
-
 public class CheckoutService {
 
     private final PriceValidatorService priceValidatorService;
@@ -36,7 +34,8 @@ public class CheckoutService {
             return new CheckoutResponse(CheckoutStatus.FAILURE, invalidCartItems);
         }
 
-        double finalPrice = calculateFinalPrice(cart);
+        //double finalPrice = calculateFinalPrice(cart);
+        double finalPrice = calculateFinalPriceReduce(cart);
         LoggerUtil.log("Final price: " + finalPrice);
         return new CheckoutResponse(CheckoutStatus.SUCCESS, finalPrice);
     }
@@ -44,6 +43,13 @@ public class CheckoutService {
     private double calculateFinalPrice(Cart cart) {
         return cart.getCartItemList().parallelStream()
                 .map(cartItem -> cartItem.getQuantity() * cartItem.getRate())
-                .collect(summingDouble(Double::doubleValue));
+                .mapToDouble(Double::doubleValue)
+                .sum();
+    }
+
+    private double calculateFinalPriceReduce(Cart cart) {
+        return cart.getCartItemList().parallelStream()
+                .map(cartItem -> cartItem.getQuantity() * cartItem.getRate())
+                .reduce(0.0, (x, y) -> x + y); // don't use IntelliJ recommendation here - it slows down
     }
 }
