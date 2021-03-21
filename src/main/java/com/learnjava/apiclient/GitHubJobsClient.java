a8b0262c0c8ffa36854d8b7363d2a07024cdaa3b
Christian Preß <cpress@it4ipm.de>
2021-03-21 18:52:26 +0100

    add RestFul API Client using Spring WebClient and CompletableFuture

---
+++ src/main/java/com/learnjava/apiclient/GitHubJobsClient.java
@@ -0,0 +1,59 @@
+package com.learnjava.apiclient;
+
+import com.learnjava.domain.github.GitHubPosition;
+import com.learnjava.util.LoggerUtil;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
+public class GitHubJobsClient {
+    private final WebClient webClient;
+
+    public GitHubJobsClient(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    public List<GitHubPosition> invokeGitHubJobsAPIWithPageNumber(int pageNumber, String description) {
+        String uriString = UriComponentsBuilder.fromUriString("/positions.json")
+                .queryParam("description", description)
+                .queryParam("page", pageNumber)
+                .buildAndExpand()
+                .toUriString();
+
+        LoggerUtil.log("uri: " + uriString);
+        List<GitHubPosition> gitHubPositions = webClient.get().uri(uriString)
+                .retrieve()
+                .bodyToFlux(GitHubPosition.class)
+                .collectList()
+                .block();
+
+        return gitHubPositions;
+    }
+
+    public List<GitHubPosition> invokeGitHubJobsAPIWithPageNumbers(List<Integer> pageNumber, String description) {
+        List<GitHubPosition> results = pageNumber.stream()
+                .map(page -> invokeGitHubJobsAPIWithPageNumber(page, description))
+                .flatMap(gitHubPositions -> gitHubPositions.stream())
+                .collect(Collectors.toList());
+
+        return results;
+    }
+
+    public List<GitHubPosition> invokeGitHubJobsAPIWithPageNumbersCompletableFuture(List<Integer> pageNumber, String description) {
+        List<CompletableFuture<List<GitHubPosition>>> resultFutures = pageNumber.stream()
+                .map(page -> CompletableFuture.supplyAsync(() -> invokeGitHubJobsAPIWithPageNumber(page, description)))
+                .collect(Collectors.toList());
+
+        List<GitHubPosition> results = resultFutures.stream()
+                .map(CompletableFuture::join)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+
+        return results;
+    }
+
+}
---
+++ src/test/java/com/learnjava/apiclient/GitHubJobsClientTest.java
@@ -0,0 +1,68 @@
+package com.learnjava.apiclient;
+
+import com.learnjava.domain.github.GitHubPosition;
+import com.learnjava.util.CommonUtil;
+import com.learnjava.util.LoggerUtil;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class GitHubJobsClientTest {
+    WebClient webClient = WebClient.create("https://jobs.github.com/");
+    GitHubJobsClient gitHubJobsClient = new GitHubJobsClient(webClient);
+
+    @Test
+    void invokeGitHubJobsAPIWithPageNumber() {
+        //given
+        int pageNum = 1;
+        String description = "JavaScript";
+
+        //when
+        List<GitHubPosition> gitHubPositions = gitHubJobsClient.invokeGitHubJobsAPIWithPageNumber(pageNum, description);
+        LoggerUtil.log("gitHubPositions " + gitHubPositions);
+
+        //then
+        assertTrue(gitHubPositions.size() > 0);
+        gitHubPositions.forEach(Assertions::assertNotNull);
+    }
+
+    @Test
+    void invokeGitHubJobsAPIWithPageNumbers() {
+        //given
+        List<Integer> pageNums = List.of(1, 2, 3);
+        String description = "Java";
+
+        CommonUtil.startTimer();
+        //when
+        List<GitHubPosition> gitHubPositions = gitHubJobsClient.invokeGitHubJobsAPIWithPageNumbers(pageNums, description);
+        CommonUtil.timeTaken();
+
+        LoggerUtil.log("gitHubPositions " + gitHubPositions);
+
+        //then
+        assertTrue(gitHubPositions.size() > 0);
+        gitHubPositions.forEach(Assertions::assertNotNull);
+    }
+
+    @Test
+    void invokeGitHubJobsAPIWithPageNumbersCompletableFuture() {
+        //given
+        List<Integer> pageNums = List.of(1, 2, 3);
+        String description = "Java";
+
+        CommonUtil.startTimer();
+        //when
+        List<GitHubPosition> gitHubPositions = gitHubJobsClient.invokeGitHubJobsAPIWithPageNumbersCompletableFuture(pageNums, description);
+        CommonUtil.timeTaken();
+
+        LoggerUtil.log("gitHubPositions " + gitHubPositions);
+
+        //then
+        assertTrue(gitHubPositions.size() > 0);
+        gitHubPositions.forEach(Assertions::assertNotNull);
+    }
+}
